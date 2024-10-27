8369fa4dddcf772586f84b844a425a4839f4d20c
Dragan Milic <dragan@netice9.com>
2024-10-27 22:18:53 +0100

    improved archive append test

--- archive/append_test.go
+++ archive/append_test.go
@@ -2,9 +2,11 @@ package archive_test
 
 import (
 	"context"
+	"os"
 	"path/filepath"
 	"testing"
 
+	"github.com/draganm/blobmap"
 	"github.com/draganm/linear/archive"
 	"github.com/draganm/statemate"
 	"github.com/neilotoole/slogt"
@@ -20,7 +22,7 @@ func TestAppend(t *testing.T) {
 	require.NoError(t, err)
 
 	for i := uint64(0); i < 100; i++ {
-		err = sm.Append(i, []byte{1, 2, 3})
+		err = sm.Append(i, []byte{1, 2, byte(i)})
 		require.NoError(t, err)
 	}
 
@@ -37,5 +39,31 @@ func TestAppend(t *testing.T) {
 
 	err = ar.Append(ctx, sm)
 	require.NoError(t, err)
+
+	blobs := filepath.Join(blobDir, "blobs")
+
+	files, err := os.ReadDir(blobs)
+	require.NoError(t, err)
+
+	require.Len(t, files, 1)
+
+	bm, err := blobmap.Open(filepath.Join(blobs, files[0].Name()))
+	require.NoError(t, err)
+
+	require.Equal(t, uint64(100), bm.LastKey()-bm.FirstKey()+1)
+
+	{
+		d, err := bm.Read(0)
+		require.NoError(t, err)
+
+		require.Equal(t, []byte{1, 2, 0}, d)
+	}
+
+	{
+		d, err := bm.Read(99)
+		require.NoError(t, err)
+
+		require.Equal(t, []byte{1, 2, 99}, d)
+	}
 
 }
